cef270b2c95a9ba41cd548bb1aeddd1b5c1d35c3
Ben Wilkinson <benwilk@microsoft.com>
2018-12-06 15:57:58 -0600

    update footer

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        BRW Azure Cloud Admin. v4
+        BRW Azure Cloud Admin. v5
       </div>
       <div class="col-lg-6">
       </div>
